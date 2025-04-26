33c035b4e2cc2fb00690ad9d8c16af641ad52379
AnjishnuSengupta <anjishnusengupta947@gmail.com>
2025-04-26 13:07:43 +0530

    Publish

---
+++ src/constants/index.js
@@ -0,0 +1,261 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  java,
+  python,
+  flutter,
+  nodejs,
+  git,
+  demo,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Student",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "Java",
+    icon: java,
+  },
+  {
+    name: "Python",
+    icon: python,
+  },
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "Flutter",
+    icon: flutter,
+  },
+];
+
+const experiences = [
+  {
+    title: "React.js Developer",
+    company_name: "Demo",
+    icon: demo,
+    iconBg: "#383E56",
+    date: "March 2020 - April 2021",
+    points: [
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos laudantium architecto mollitia officiis ad temporibus,",
+      "aliquid dolore dicta quasi eaque excepturi sint dolorem possimus nulla voluptas reiciendis,",
+      "non accusamus ipsam minus distinctio quia pariatur? Corporis odio ducimus aliquid culpa!",
+      "Dicta quibusdam quaerat possimus aperiam natus numquam, doloremque minus est? Deserunt ab magni nesciunt nisi amet aliquid,", 
+      "consequatur perferendis atque rerum dolorem earum sunt.",
+    ],
+  },
+  {
+    title: "React Native Developer",
+    company_name: "Demo",
+    icon: demo,
+    iconBg: "#E6DEDD",
+    date: "Jan 2021 - Feb 2022",
+    points: [
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos laudantium architecto mollitia officiis ad temporibus,",
+      "aliquid dolore dicta quasi eaque excepturi sint dolorem possimus nulla voluptas reiciendis,",
+      "non accusamus ipsam minus distinctio quia pariatur? Corporis odio ducimus aliquid culpa!",
+      "Dicta quibusdam quaerat possimus aperiam natus numquam, doloremque minus est? Deserunt ab magni nesciunt nisi amet aliquid,", 
+      "consequatur perferendis atque rerum dolorem earum sunt.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Demo",
+    icon: demo,
+    iconBg: "#383E56",
+    date: "Jan 2022 - Jan 2023",
+    points: [
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos laudantium architecto mollitia officiis ad temporibus,",
+      "aliquid dolore dicta quasi eaque excepturi sint dolorem possimus nulla voluptas reiciendis,",
+      "non accusamus ipsam minus distinctio quia pariatur? Corporis odio ducimus aliquid culpa!",
+      "Dicta quibusdam quaerat possimus aperiam natus numquam, doloremque minus est? Deserunt ab magni nesciunt nisi amet aliquid,", 
+      "consequatur perferendis atque rerum dolorem earum sunt.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+    company_name: "Demo",
+    icon: demo,
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Present",
+    points: [
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos laudantium architecto mollitia officiis ad temporibus,",
+      "aliquid dolore dicta quasi eaque excepturi sint dolorem possimus nulla voluptas reiciendis,",
+      "non accusamus ipsam minus distinctio quia pariatur? Corporis odio ducimus aliquid culpa!",
+      "Dicta quibusdam quaerat possimus aperiam natus numquam, doloremque minus est? Deserunt ab magni nesciunt nisi amet aliquid,", 
+      "consequatur perferendis atque rerum dolorem earum sunt.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem velit molestiae ab non, nam, suscipit iusto odio placeat modi repellat eum?",
+    name: "Demo",
+    designation: "CFO",
+    company: "xxxx",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem velit molestiae ab non, nam, suscipit iusto odio placeat modi repellat eum?",
+    name: "Demo",
+    designation: "COO",
+    company: "xxxx",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem velit molestiae ab non, nam, suscipit iusto odio placeat modi repellat eum?",
+    name: "Demo",
+    designation: "CTO",
+    company: "xxxx",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "BackRooms UI",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem velit molestiae ab non, nam, suscipit iusto odio placeat modi repellat eum? Molestiae, at. Laborum nihil quibusdam maiores labore nesciunt suscipit.",
+    tags: [
+      {
+        name: "html",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css",
+        color: "green-text-gradient",
+      },
+      {
+        name: "js",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/BackRooms-UI",
+  },
+  {
+    name: "GoDaddy",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem velit molestiae ab non, nam, suscipit iusto odio placeat modi repellat eum? Molestiae, at. Laborum nihil quibusdam maiores labore nesciunt suscipit.",
+    tags: [
+      {
+        name: "html",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css",
+        color: "green-text-gradient",
+      },
+      {
+        name: "js",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/GoDaddy",
+  },
+  {
+    name: "Sasta Watch",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem velit molestiae ab non, nam, suscipit iusto odio placeat modi repellat eum? Molestiae, at. Laborum nihil quibusdam maiores labore nesciunt suscipit.",
+    tags: [
+      {
+        name: "html",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css",
+        color: "green-text-gradient",
+      },
+      {
+        name: "js",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/Sasta-Watch",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
